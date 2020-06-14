e8f7b69f26dafbc8b3b595452e0c573283998e45
Ronaldo Cardozo <ronaldo.cardozo@wardenautomation.com>
2020-06-13 22:27:56 -0400

    Edit sh

--- train-version-slim.sh
+++ train-version-slim.sh
@@ -6,9 +6,9 @@ mkdir -p "$log_dir"
 
 python3 -u train.py \
   --datasets \
-  ./data/wider_face_add_lm_10_10 \
+  ./data \
   --validation_dataset \
-  ./data/wider_face_add_lm_10_10 \
+  ./data \
   --net \
   slim \
   --num_epochs \
